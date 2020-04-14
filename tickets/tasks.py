f51eda8e7dc85d013af88548c87e251875482fd0
Afeez Aziz <afeezaziz@gmail.com>
2020-04-14 19:41:22 +0800

    das

--- tickets/tasks.py
+++ tickets/tasks.py
@@ -3,6 +3,7 @@ from celery import task
 from celery.decorators import periodic_task
 from celery.task.schedules import crontab
 
+from requests
 
 
 @periodic_task(run_every=(crontab()), name="vehicle_custom_send_data")
@@ -10,5 +11,7 @@ def lol():
 
     asd = 'Running'
     print(asd)
+
+    req = requests.get('https://webhook.site/2f3532f4-24f1-41e6-952d-b9482ebcd3b1')
 
     return asd
