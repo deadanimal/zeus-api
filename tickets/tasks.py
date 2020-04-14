92400944bc92b2c23423649496cebc0fad7b0a20
Afeez Aziz <afeezaziz@gmail.com>
2020-04-14 21:39:02 +0800

    dsa

--- tickets/tasks.py
+++ tickets/tasks.py
@@ -5,12 +5,11 @@ from celery.task.schedules import crontab
 
 import requests
 
-@periodic_task(run_every=(crontab()), name="check_flespi_galileosky_events", ignore_result=True)
+@periodic_task(run_every=(crontab()), ignore_result=True)
 def lol():
-
-    asd = 'Running'
-    print(asd)
 
     link_provided = 'https://webhook.site/2f3532f4-24f1-41e6-952d-b9482ebcd3b1'
     r = requests.get(link_provided)
+
+    return True
     
