0c5eed6ff0aa37a65999deba8887c2ebe95d5c14
Afeez Aziz <afeezaziz@gmail.com>
2020-04-14 21:29:05 +0800

    dsa

--- zeusapi/celery.py
+++ zeusapi/celery.py
@@ -10,6 +10,7 @@ from django.conf import settings
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'zeusapi.settings')
 
 app = Celery('zeusapi')
-app.config_from_object('django.conf:settings', namespace='CELERY')
+app.config_from_object('django.conf:settings')
+#app.config_from_object('django.conf:settings', namespace='CELERY')
 app.autodiscover_tasks(lambda: settings.INSTALLED_APPS)
 
